f58911155042748b2410aff1cb8b18591e7c1b85
alexbelz123 <belz.alexx@gmail.com>
2024-12-23 01:34:35 +0200

    list snake animation fade in

--- app/routes/_index/components/FinanceSection/TabsStepper.tsx
+++ app/routes/_index/components/FinanceSection/TabsStepper.tsx
@@ -1,29 +1,41 @@
-import { FC } from 'react';
-import { ListSnake } from '~/lib/organisms/ListSnake';
+import { FC, useEffect, useState } from "react";
+import { ListSnake } from "~/lib/organisms/ListSnake";
+
+import styles from "./financeSection.module.css";
+import clsx from "clsx";
 
 type TabsStepperProps = { tabId: string };
 
 const financeStepperContent: StringRecord<string[]> = {
   buying: [
-    'Buy RWA shares',
-    'Add to Personal Vault',
-    'Borrow Instantly',
-    'Buy More RWA shares',
-    'Multiply Your Dividends',
+    "Buy RWA shares",
+    "Add to Personal Vault",
+    "Borrow Instantly",
+    "Buy More RWA shares",
+    "Multiply Your Dividends",
   ],
-  selling: ['Sell RWA shares', 'Sell Instantly', 'Sell More RWA shares'],
+  selling: ["Sell RWA shares", "Sell Instantly", "Sell More RWA shares"],
   borrowing: [
-    'Buy RWA Shares',
-    'Add to Personal Vault',
-    'Borrow Instantly',
-    'Buy More RWA Shares',
-    'Multiply Your Dividends',
+    "Buy RWA Shares",
+    "Add to Personal Vault",
+    "Borrow Instantly",
+    "Buy More RWA Shares",
+    "Multiply Your Dividends",
   ],
 };
 
 export const TabsStepper: FC<TabsStepperProps> = ({ tabId }) => {
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => {
+    setPlay(true);
+  }, [tabId]);
+
   return (
-    <div>
+    <div
+      className={clsx(play && styles.snakeWrapper)}
+      onAnimationEnd={() => setPlay(false)}
+    >
       <ListSnake items={financeStepperContent[tabId]} />
     </div>
   );
