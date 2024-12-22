71e0dcdb2f8bb11714c59d67b6205c85efa32258
alexbelz123 <belz.alexx@gmail.com>
2024-12-22 19:29:48 +0200

    updated FAQ content

---
+++ app/routes/_index/index.const.tsx
@@ -0,0 +1,107 @@
+export const homeFAQ = [
+  {
+    title: "What is Equiteez?",
+    description:
+      "Equiteez is a digital platform that allows investors to purchase fractional shares of fully compliant, income-generating assets worldwide, starting from as little as $50.",
+  },
+  {
+    title: "How does Equiteez work?",
+    description:
+      "Equiteez tokenizes assets, allowing investors to buy shares (tokens) that represent partial ownership of these properties. Investors can earn rental income and benefit from property appreciation.",
+  },
+  {
+    title: "What types of assets are available for investment on Equiteez?",
+    description:
+      "Equiteez offers a diverse range of assets, including residential, commercial, tokenized debt, RWA bonds, Bitcoin miners, and insurance-linked securities.",
+  },
+  {
+    title: "How can I buy RWAs on Equiteez?",
+    description: (
+      <section>
+        <div>
+          Buying RWA (Real World Asset) shares on Equiteez is a simple process:
+        </div>
+        <ol className="list-disc space-y-4 pl-6 my-6 sm:pl-4 sm:my-4 sm:space-y-2">
+          <li>Visit the Equiteez marketplace to explore available assets.</li>
+          <li>
+            Select the asset or property you're interested in. Remember, you can
+            diversify your portfolio by investing in multiple assets.
+          </li>
+          <li>
+            Decide how much you want to invest. You can start with as little as
+            $50 per property.
+          </li>
+          <li>
+            Carefully examine the asset information, including location,
+            expected returns, and any associated risks.
+          </li>
+          <li>
+            Make sure you understand the investment terms, including potential
+            returns, slippage, fees, and any restrictions.
+          </li>
+          <li>
+            Follow the prompts to finalize your investment. This may include
+            connecting your digital wallet or providing payment information.
+          </li>
+        </ol>
+        <div>
+          Once the transaction is complete, you'll receive confirmation of your
+          new RWA shares.
+        </div>
+      </section>
+    ),
+  },
+  {
+    title: "How Can I sell the assets that I bought?",
+    description: (
+      <section>
+        <div>
+          Yes, you have multiple options for selling your assets across three
+          different decentralized exchanges:
+        </div>
+        <ol className="list-disc space-y-4 pl-6 my-6 sm:pl-4 sm:my-4 sm:space-y-2">
+          <li>
+            Over The Counter (OTC): Post a bulletin to sell your RWA directly
+            through peer-to-peer (P2P) transactions.
+          </li>
+          <li>
+            Automated Market Maker (AMM): Sell your assets directly into the AMM
+            at the current market price.
+          </li>
+          <li>
+            Order Book Exchange: Place a limit order to sell your assets when
+            they reach your desired price point.
+          </li>
+        </ol>
+        <div className="italic">
+          Please note that not all assets may be immediately liquid.
+        </div>
+      </section>
+    ),
+  },
+  {
+    title: "Can I borrow against my assets?",
+    description:
+      "Yes, you can borrow against your assets through the integration with Maven Finance.",
+  },
+  {
+    title: "How do I earn returns on my investment?",
+    description:
+      "Investors can earn returns through regular rental income payments and potential asset value appreciation when they sell their shares.",
+  },
+  {
+    title: "How do I receive RWA dividends?",
+    description:
+      "Dividends are paid out monthly to your wallet. You can also collect property appreciation when you cash out as the properties grow in value over time.",
+  },
+  {
+    title: "What is the Maven Finance lending platform?",
+    description:
+      "Maven Finance is a decentralized finance (DeFi) platform integrated with Equiteez that allows investors to borrow against their tokenized real estate portfolio.",
+  },
+  {
+    title: "What control do I have over my investments?",
+    description:
+      "You have full control of your investments with zero lock-in periods and full transparency reports. You can reinvest your rental income, sell whenever you like, or get a loan in minutes to leverage your portfolio.",
+  },
+];
