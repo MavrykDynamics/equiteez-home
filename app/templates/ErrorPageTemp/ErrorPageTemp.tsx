10ad85da7867c6c2ad1994d034c0c043c473bf74
alexbelz123 <belz.alexx@gmail.com>
2024-12-23 13:51:44 +0200

    404 finished

--- app/templates/ErrorPageTemp/ErrorPageTemp.tsx
+++ app/templates/ErrorPageTemp/ErrorPageTemp.tsx
@@ -9,9 +9,11 @@ import {
 } from "~/providers/ToasterProvider/toaster.provider.const";
 
 import Error404Img from "app/assets/error/404.png";
+import ErrorBgMobile from "app/assets/error/bg-mobile.webp";
 
 import styles from "./errorPageTemp.module.css";
 import { Socials } from "~/a11y/Socials/Socials";
+import clsx from "clsx";
 
 type ErrorPageProps = {
   headerText?: string;
@@ -25,34 +27,41 @@ export const ErrorPageTemp = ({
   type = ERROR_TYPE_FATAL,
 }: ErrorPageProps) => {
   return (
-    <>
-      <div className="h-screen overflow-hidden  bg-mvrk font-aeonik text-mvrk-main gap-3 flex items-center justify-between flex-col w-screen bg-sand-50">
-        <div className={styles.headerWrapper}>
-          <Header />
+    <section
+      className={clsx(
+        "h-screen overflow-hidden font-aeonik text-mvrk-main gap-3 flex items-center justify-between flex-col w-screen bg-sand-50",
+        styles.errorTempContainer
+      )}
+    >
+      <div className={styles.headerWrapper}>
+        <Header />
+      </div>
+      <div className={styles.contentWrapper}>
+        <img className={styles.foourOfour} src={Error404Img} alt="404" />
+        <div className="flex flex-col gap-4 items-center text-sand-900 px-8">
+          <h3 className={styles.errorHeader}>
+            {headerText || "Unknown error"}
+          </h3>
+          <p className={styles.errorDesc}>{descText}</p>
         </div>
-        <div className={styles.contentWrapper}>
-          <img className={styles.foourOfour} src={Error404Img} alt="404" />
-          <div className="flex flex-col gap-4 items-center text-sand-900 px-8">
-            <h3 className={styles.errorHeader}>
-              {headerText || "Unknown error"}
-            </h3>
-            <p className={styles.errorDesc}>{descText}</p>
-          </div>
-          {type === ERROR_TYPE_ROUTER && (
-            <a href="/" className="mvrk-btn mx-auto block w-fit mt-6">
-              <Button> Go To Main Page</Button>
-            </a>
-          )}
+        {type === ERROR_TYPE_ROUTER && (
+          <a href="/" className="mvrk-btn mx-auto block w-fit mt-6">
+            <Button> Go To Main Page</Button>
+          </a>
+        )}
 
-          <div className={styles.socials}>
-            <Socials />
-          </div>
-        </div>
-
-        <div className={styles.footerWrapper}>
-          <FooterSecondary />
+        <div className={styles.socials}>
+          <Socials />
         </div>
       </div>
-    </>
+
+      <div className={styles.footerWrapper}>
+        <FooterSecondary />
+      </div>
+
+      <div className={styles.errorBgMobile}>
+        <img src={ErrorBgMobile} alt="error town" />
+      </div>
+    </section>
   );
 };
