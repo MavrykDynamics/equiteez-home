1e7c3e4e97eb4a7c7037752f8cad608f8a9f8b38
alexbelz123 <belz.alexx@gmail.com>
2024-12-24 15:08:03 +0200

    postcss-import

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,15 +1,7 @@
-const postcss = require("postcss");
-const postcssEnvFunction = require("postcss-env-function");
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const path = require("path");
-
-const config = postcss([
-  autoprefixer,
-  tailwindcss,
-  postcssEnvFunction({
-    importFrom: [path.resolve("./app/styles/vars.json")],
-  }),
-]);
-
-module.exports = config;
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
